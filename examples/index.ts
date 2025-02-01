d09e42b060122a9804394361e3cb385b8672ac68
unknown <oli.millington@gmail.com>
2025-02-01 12:06:02 +0700

    update example

--- examples/index.ts
+++ examples/index.ts
@@ -1,42 +1,34 @@
-// import { css, html, render, router, routerRedirect } from "../src";
+import { css, html, render, router, routerRedirect } from "../src";
 
-// const { nav, a } = html;
+const { nav, a } = html;
 
-// router.start({
-//   "/bench": async () => {
-//     const { BenchApp } = await import("./benchmark/app");
-//     render(BenchApp);
-//   },
-//   "/todo": async () => {
-//     const { TodoApp } = await import("./todo/app");
-//     render(TodoApp);
-//   },
-// });
-
-// routerRedirect("/", "/bench");
-
-// render(
-//   nav(
-//     {
-//       mount: "nav",
-//       css: css({
-//         padding: 10,
-//         a: {
-//           cursor: "pointer",
-//         },
-//       }),
-//     },
-//     [
-//       a({ onclick: () => router.navigate("/bench") }, "Benchmark"),
-//       a({ onclick: () => router.navigate("/todo") }, () => "Todo"),
-//     ]
-//   )
-// );
-
-import { render } from "../src";
-
-render({
-  mount: "app",
-  tag: "h1",
-  children: "Hello, World!",
+router.start({
+  "/bench": async () => {
+    const { BenchApp } = await import("./benchmark/app");
+    render(BenchApp);
+  },
+  "/todo": async () => {
+    const { TodoApp } = await import("./todo/app");
+    render(TodoApp);
+  },
 });
+
+routerRedirect("/", "/bench");
+
+render(
+  nav(
+    {
+      mount: "nav",
+      css: css({
+        padding: 10,
+        a: {
+          cursor: "pointer",
+        },
+      }),
+    },
+    [
+      a({ onclick: () => router.navigate("/bench") }, "Benchmark"),
+      a({ onclick: () => router.navigate("/todo") }, () => "Todo"),
+    ]
+  )
+);
