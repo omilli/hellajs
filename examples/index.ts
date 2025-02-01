a482a310dc3f6bd2715502b81320f64fda32a11c
unknown <oli.millington@gmail.com>
2025-02-01 20:11:37 +0700

    update example

--- examples/index.ts
+++ examples/index.ts
@@ -1,3 +1,36 @@
-import { render } from "../src";
+import { css, html, render, router, routerRedirect } from "../src";
 
-render({ tag: "div", mount: "app", children: "Hello World!" });
+const { nav, a } = html;
+
+router.start({
+  "/": "/bench",
+  "/bench": async () => {
+    const { BenchApp } = await import("./benchmark/app");
+    render(BenchApp);
+    return () => console.log("cleanup bench");
+  },
+  "/todo": async () => {
+    const { TodoApp } = await import("./todo/app");
+    render(TodoApp);
+  },
+});
+
+routerRedirect("/", "/bench");
+
+render(
+  nav(
+    {
+      mount: "nav",
+      css: css({
+        padding: 10,
+        a: {
+          cursor: "pointer",
+        },
+      }),
+    },
+    [
+      a({ onclick: () => router.navigate("/bench") }, "Benchmark"),
+      a({ onclick: () => router.navigate("/todo") }, () => "Todo"),
+    ]
+  )
+);
