9b715182e0694f29f6a29e8dc0597218d1b97a8c
unknown <oli.millington@gmail.com>
2025-02-02 21:04:25 +0700

    cleanup repo

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 // vite.config.ts
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
+  plugins: [dts({ include: ["src"] })],
   build: {
     target: "esnext",
+    minify: "esbuild",
+    lib: {
+      entry: resolve(__dirname, "lib/index.ts"),
+      name: "hella",
+      fileName: (format) => `index.${format}.js`,
+      formats: ["es", "umd", "cjs"],
+    },
   },
   esbuild: {
     pure: ["console.warn", "console.error"],
