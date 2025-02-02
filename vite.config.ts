6f3666367c825d14e0ad20b8cdc0b3e5b3f89a20
unknown <oli.millington@gmail.com>
2025-02-02 20:15:02 +0700

    remove old deps

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     target: "esnext",
-    minify: "terser",
   },
   esbuild: {
     pure: ["console.warn", "console.error"],
