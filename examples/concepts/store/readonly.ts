bd97f43ece8df6ff85747791ebe3c96fb29beecd
unknown <oli.millington@gmail.com>
2025-02-08 20:22:04 +0700

    readonly keys  store

--- examples/concepts/store/readonly.ts
+++ examples/concepts/store/readonly.ts
@@ -3,19 +3,19 @@ import { store } from "../../../lib";
 interface AppStore {
   readProp: number;
   mutableProp: number;
-  setImmutable: (value: number) => void;
+  setReadProp: (value: number) => void;
 }
 
 const appStore = store<AppStore>(
   (state) => ({
     readProp: 0,
     mutableProp: 0,
-    setImmutable: (value: number) => state.readProp.set(value),
+    setReadProp: (value: number) => state.readProp.set(value),
   }),
   { readonly: ["readProp"] } // Set true for all properties
 );
 
 appStore.readProp.set(2); // Warning: Cannot modify readonly store signal
 appStore.mutableProp.set(5);
-appStore.setImmutable(10);
+appStore.setReadProp(10);
 console.log(appStore.readProp(), appStore.mutableProp()); // 10, 5
