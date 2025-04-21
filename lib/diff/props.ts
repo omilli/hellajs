31117b6727f891a0ae7c134e16846efd0918cf79
omilli <oli.millington@gmail.com>
2025-04-21 20:52:32 +0700

    fix(attributes, props): remove handling of 'key' attribute during processing

--- lib/diff/props.ts
+++ lib/diff/props.ts
@@ -25,7 +25,7 @@ export function propProcessor<T extends HTMLTagName = HTMLTagName>(
 		const value = props[key];
 		switch (true) {
 			// break immediately if the key starts with "on"
-			case key.startsWith("on") || key === "key":
+			case key.startsWith("on"):
 				break;
 			case key === "className":
 				// Handle className separately
