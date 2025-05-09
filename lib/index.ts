9b11db1b5f09e9293eca8c35d4ba89e354124353
omilli <oli.millington@gmail.com>
2025-05-09 21:27:25 +0700

    fix: 0.6.1 index

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,7 @@
 export { signal, batch, untracked, type Signal, effect, store, type Store } from './reactive';
 export { html } from './html';
 export { mount } from './mount';
-export { registerDelegatedEvent, setNodeHandler } from "./events";
+export { setNodeHandler } from "./events";
 export { forEach } from "./forEach";
 export { show } from "./show";
 export * from './types/dom';
