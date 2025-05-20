f00d5739975660d8eb9878d0aea07b045caa9c84
omilli <oli.millington@gmail.com>
2025-05-20 08:36:33 +0700

    refactor: remove context implementation and update exports in dom and index files

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
-export { context, forEach, html, mount, show } from "./dom";
+export { forEach, html, mount, show } from "./dom";
 export { signal, computed, batch, untracked, effect, store } from './reactive';
 export { router, navigate, route } from './router';
 export { resource } from './reactive/resource';
