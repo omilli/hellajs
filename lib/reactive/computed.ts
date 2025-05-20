2d7930aa413d483d568d80bfb88e93e9ef5127f8
omilli <oli.millington@gmail.com>
2025-05-20 21:14:10 +0700

    chore(types): enhance ReadonlySignal interface and update Resource type definitions

--- lib/reactive/computed.ts
+++ lib/reactive/computed.ts
@@ -1,8 +1,9 @@
 import { signal } from "./signal";
 import { effect } from "./effect";
 import { untracked } from "./untracked";
+import type { ReadonlySignal } from "../types";
 
-export function computed<T>(compute: () => T) {
+export function computed<T>(compute: () => T): ReadonlySignal<T> {
   const result = signal<T>(untracked(compute));
   let lastValue = result();
 
@@ -14,5 +15,5 @@ export function computed<T>(compute: () => T) {
     }
   });
 
-  return result;
+  return result as ReadonlySignal<T>;
 }
