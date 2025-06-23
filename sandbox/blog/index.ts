0277dda93dba754cd64ddf2d0ee7996da6557b9b
omilli <oli.millington@gmail.com>
2025-06-23 16:03:40 +0700

    chore: revert blog example

--- sandbox/blog/index.ts
+++ sandbox/blog/index.ts
@@ -1,13 +1,1 @@
-// import "./router";
-
-import { effect, signal } from "../../packages/core"
-import { html, mount } from "../../packages/dom";
-
-function App() {
-  const counter = signal(0);
-  setInterval(() => counter(counter() + 1), 1000);
-  effect(() => console.log("Counter updated:", counter()));
-  return html.div(() => counter());
-}
-
-mount(App, "#app");
+import "./router";
