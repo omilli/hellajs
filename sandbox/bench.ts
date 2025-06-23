880cc986fb10efd9cbf15fe00a65a1eb0026c91c
omilli <oli.millington@gmail.com>
2025-06-23 12:23:14 +0700

    chore: update import path for dom library

--- sandbox/bench.ts
+++ sandbox/bench.ts
@@ -1,5 +1,5 @@
 import { signal, type Signal, batch } from "../packages/core";
-import { html, forEach, mount } from "../packages/dom/lib";
+import { html, forEach, mount } from "../packages/dom";
 
 const { div, table, tbody, tr, td, button, span, a, h1 } = html;
 
