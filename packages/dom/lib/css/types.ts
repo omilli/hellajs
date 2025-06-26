d177c42043fd2e774df1fd9e3ab7ed09e147100f
omilli <oli.millington@gmail.com>
2025-06-26 07:13:47 +0700

    fix(dom): add generic slector for css

--- packages/dom/lib/css/types.ts
+++ packages/dom/lib/css/types.ts
@@ -20,9 +20,10 @@ export type CSSSelector =
   | keyof DollarTagSelectors
   | CSS.AtRules
   | CSS.Pseudos
+  | (string & {});
 
 export type CSSObject = {
   [key in CSSSelector]?: CSSValue;
 } & {
   [K in keyof CSS.Properties]?: CSS.Properties[K] | string | number;
-}
+};
