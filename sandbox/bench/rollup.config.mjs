9e7597feac46d9d2f97a2d3467e392774200ba87
omilli <oli.millington@gmail.com>
2025-06-27 09:19:36 +0700

    chore: change bench to jsx

---
+++ sandbox/bench/rollup.config.mjs
@@ -0,0 +1,39 @@
+import rollupHellaJS from '../../plugins/rollup/index.mjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+
+export default {
+  input: 'src/main.jsx',
+  output: {
+    file: 'dist/main.js',
+    format: 'iife',
+  },
+  plugins: [
+    rollupHellaJS(),
+    resolve(),
+    terser({
+      compress: {
+        passes: 3,
+        drop_console: true,
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        unsafe_math: true,
+        unsafe_methods: true,
+        unsafe_proto: true,
+        unsafe_regexp: true,
+        unsafe_undefined: true,
+      },
+      mangle: {
+        toplevel: true,
+        properties: {
+          regex: /^_/,
+        },
+      },
+      format: {
+        comments: false,
+      },
+    }),
+  ],
+};
