6e2398241ae51bd61e1ab6fe7f54c785e2ec986e
omilli <oli.millington@gmail.com>
2025-06-27 12:08:15 +0700

    fix(dom): remove show and css

--- packages/dom/lib/index.ts
+++ packages/dom/lib/index.ts
@@ -1,12 +1,10 @@
 import type { HTMLAttributeMap, VNode } from "./types";
 
-export * from "./css"
 export * from "./events";
 export * from "./forEach";
 export * from "./html";
 export * from "./mount";
 export * from "./registry";
-export * from "./show";
 export * from "./types";
 
 declare global {
