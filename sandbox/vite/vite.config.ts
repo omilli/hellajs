ba9e1b1b0f543addf848d18beb4637ca332e1599
omilli <oli.millington@gmail.com>
2025-06-28 13:01:48 +0700

    chore(vite): update dependencies and integrate vite-plugin-hellajs

--- sandbox/vite/vite.config.ts
+++ sandbox/vite/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import viteHellaJS from "../../plugins/vite";
+import viteHellaJS from "vite-plugin-hellajs";
 
 export default defineConfig({
   plugins: [viteHellaJS()],
