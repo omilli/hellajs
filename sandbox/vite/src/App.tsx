ffa71ee8fbaaf64e306c193c03b7f71f98cd2656
omilli <oli.millington@gmail.com>
2025-06-29 09:07:24 +0700

    chore(sandbox): test consume patterns

--- sandbox/vite/src/App.tsx
+++ sandbox/vite/src/App.tsx
@@ -1,34 +1,86 @@
-import { signal } from "@hellajs/core";
-import { css } from "@hellajs/css";
+import { Signal, signal } from "@hellajs/core";
+import { html, VNodeProps, VNodeValue } from "../../../packages/dom/lib";
 
-export const Foo = ({ title }: { title: string }, children?: JSX.Element) => {
-  return (
-    <div class={css({
-      backgroundColor: "red",
-      color: "white",
-      padding: "10px",
-    })}>
-      <h1>{title}</h1>
-      {children}
-    </div>
-  );
-};
+type HoleArgs = (VNodeProps | VNodeValue)[];
 
-export const App = () => {
-  const counter = signal(0);
+type HoleChild = (...args: HoleArgs) => VNodeValue;
 
-  const increment = () => {
-    counter(counter() + 1);
+interface MenuProps {
+  isOpen: Signal<boolean>;
+}
+
+interface MenuProvider extends MenuProps {
+  toggle: () => void;
+  Container: HoleChild;
+  Button: HoleChild;
+  List: HoleChild;
+  Item: HoleChild;
+}
+
+const menuProps = {
+  isOpen: signal(false)
+}
+
+function Menu({ isOpen }: MenuProps = menuProps): MenuProvider {
+  const menu = {
+    isOpen,
+    toggle: () => menu.isOpen(!menu.isOpen())
+  } as MenuProvider;
+
+  menu.Container = (...args: HoleArgs) =>
+    html.div({ "data-ui": "menu" },
+      hole(args).children
+    );
+
+  menu.Button = (...args: HoleArgs) => {
+    const { props, children } = hole(args);
+
+    return html.button({
+      ...props,
+      "data-ui": "menu-button",
+      onclick: (event) => {
+        menu.toggle();
+        props.onclick?.(event);
+      }
+    }, children)
   };
 
-  return (
-    <div class={() => counter() % 2 === 0 ? "even" : "odd"}>
-      <Foo title="Tag"><p>{counter}</p><p>{counter}</p></Foo>
+  menu.List = (...args: HoleArgs) =>
+    html.ul({ "data-ui": "menu-items" },
+      hole(args).children
+    );
 
-      {
-        () => counter() % 2 === 0 ? <p>Even</p> : <p>Odd</p>
-      }
-      <button onClick={increment}>Increment</button>
-    </div>
+  menu.Item = (...args: HoleArgs) =>
+    html.li({ "data-ui": "menu-item" },
+      hole(args).children
+    );
+
+  return menu;
+};
+
+const hole = (args: any[]) => {
+  const isProps = typeof args[0] === 'object' && Object.hasOwn(args[0], 'tag');
+  let props: VNodeProps = isProps ? args[0] : {}, children = args;
+  return { props, children }
+}
+
+export const App = () => {
+  const UserSelect = Menu();
+
+  return (
+    <>
+      {UserSelect.Container(
+        UserSelect.Button({ onclick: () => console.log("foo") },
+          "Toggle Menu"
+        ),
+        () => UserSelect.isOpen() &&
+          UserSelect.List(
+            UserSelect.Item("Item 1"),
+            UserSelect.Item("Item 2"),
+            UserSelect.Item("Item 3"),
+          )
+      )}
+    </>
   )
 };
+
--- sandbox/vite/src/main.ts
+++ sandbox/vite/src/main.ts
@@ -1,4 +1,4 @@
 import { mount } from "@hellajs/dom";
 import { App } from "./App";
-
+import { Consume } from "./Consume";
 mount(App)
