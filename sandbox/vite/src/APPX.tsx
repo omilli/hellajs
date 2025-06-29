4eeee996e9b82e3bae193b147312ad96a47b3d77
omilli <oli.millington@gmail.com>
2025-06-29 23:22:58 +0700

    chore: headless

---
+++ sandbox/vite/src/APPX.tsx
@@ -0,0 +1,38 @@
+import { Signal, signal, } from "@hellajs/core";
+import { HeadlessAccordion } from "./ui/Accordion";
+import { forEach } from "@hellajs/dom";
+
+export const AppX = () => {
+
+  const AccordionItems = [
+    {
+      title: "Item 1",
+      content: "Content for Item 1"
+    },
+    {
+      title: "Item 2",
+      content: "Content for Item 2"
+    },
+    {
+      title: "Item 3",
+      content: "Content for Item 3"
+    }
+  ]
+
+  return (
+    <>
+      {
+        forEach(AccordionItems, (item) => {
+          const { Container, Toggle, Item } = HeadlessAccordion();
+          return (
+            <Container>
+              <Toggle clickEvent={() => console.log(item.title)}>{item.title}</Toggle>
+              <Item>{item.content}</Item>
+            </Container>
+          )
+        })
+      }
+    </>
+  )
+};
+
