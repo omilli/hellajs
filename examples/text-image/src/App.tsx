bf5dc2b89fba58293202775775037c453a99ad85
omilli <oli.millington@gmail.com>
2025-08-10 21:32:21 +0700

    chore: update text image example

--- examples/text-image/src/App.tsx
+++ examples/text-image/src/App.tsx
@@ -1,7 +1,8 @@
 import { signal } from "@hellajs/core";
 import { router } from "@hellajs/router";
-import { mount, slot } from "@hellajs/dom";
+import { mount } from "@hellajs/dom";
 import { css } from "@hellajs/css";
+
 import { Header } from "./components/Header";
 
 
@@ -35,32 +36,10 @@ css({
   }
 }, { global: true });
 
-const Counter = () => {
-  const count = signal(0);
-  const Provider = slot((props, children) => (
-    <div {...props}>
-      {children}
-      <p>Count: {() => count()}</p>
-      <button onclick={() => count(count() + 1)}>Increment</button>
-    </div>
-  ));
-  return { Provider, count };
-}
-
 
 const App = () => {
-  const Counter1 = Counter();
-  const Counter2 = Counter();
   return (
     <>
-      <Counter1.Provider class="counter-provider">
-        <p>Count1: {() => Counter1.count()}</p>
-        <button onclick={() => Counter1.count(Counter1.count() + 1)}>Increment</button>
-      </Counter1.Provider>
-      <Counter2.Provider class="counter-provider">
-        <p>Count2: {() => Counter2.count()}</p>
-        <button onclick={() => Counter2.count(Counter2.count() + 1)}>Increment</button>
-      </Counter2.Provider>
       <Header />
       {/*
         Don't call page()
