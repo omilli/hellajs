252cd48b121d1e5ae648ec4ef82874d6f01d02e5
GitHub Action <action@github.com>
2025-08-21 21:06:46 +0700

    docs: update build type

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,9 +6,6 @@ import mdx from '@astrojs/mdx';
 import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
-  build: {
-    format: "file",
-  },
   integrations: [icon(), mdx(), pagefind()],
   vite: {
     plugins: [tailwindcss()],
