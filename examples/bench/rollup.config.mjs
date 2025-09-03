c563252c80d69969229b2abd03eb5c984bc98a93
omilli <oli.millington@gmail.com>
2025-09-03 08:57:01 +0700

    chore: bench example to ts

--- examples/bench/rollup.config.mjs
+++ examples/bench/rollup.config.mjs
@@ -3,7 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 
 export default {
-  input: 'src/main.jsx',
+  input: 'src/main.tsx',
   output: {
     file: 'dist/main.js',
     format: 'iife',
@@ -11,28 +11,28 @@ export default {
   plugins: [
     rollupHellaJS(),
     resolve(),
-    terser({
-      compress: {
-        passes: 3,
-        drop_console: true,
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        unsafe_math: true,
-        unsafe_methods: true,
-        unsafe_proto: true,
-        unsafe_regexp: true,
-        unsafe_undefined: true,
-      },
-      mangle: {
-        toplevel: true,
-        properties: {
-          regex: /^_/,
-        },
-      },
-      format: {
-        comments: false,
-      },
-    }),
+    // terser({
+    //   compress: {
+    //     passes: 3,
+    //     drop_console: true,
+    //     pure_getters: true,
+    //     unsafe: true,
+    //     unsafe_comps: true,
+    //     unsafe_math: true,
+    //     unsafe_methods: true,
+    //     unsafe_proto: true,
+    //     unsafe_regexp: true,
+    //     unsafe_undefined: true,
+    //   },
+    //   mangle: {
+    //     toplevel: true,
+    //     properties: {
+    //       regex: /^_/,
+    //     },
+    //   },
+    //   format: {
+    //     comments: false,
+    //   },
+    // }),
   ],
 };
