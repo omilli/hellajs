ab48edc5fc87b6852e2eea52708238d69e78eb68
GitHub Action <action@github.com>
2025-09-12 16:56:47 +0700

    docs: migrate to packages

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,10 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import mdx from '@astrojs/mdx';
 import pagefind from "astro-pagefind";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon(), mdx(), pagefind()],
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@packages': '../packages'
+      }
+    }
   },
 });
