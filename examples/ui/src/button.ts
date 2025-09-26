b80431decc6bb9bb06a8f7e41a2229395bfe36da
omilli <oli.millington@gmail.com>
2025-09-26 19:41:49 +0700

    chore(ui): cleanup

--- examples/ui/src/button.ts
+++ examples/ui/src/button.ts
@@ -1,4 +1,4 @@
-import { css, type CSSObject } from "../../../packages/css";
+import { css, type CSSObject } from "@hellajs/css";
 import { colors, type ColorKey } from "./color";
 import { scale } from "./global";
 import { size } from "./utils";
@@ -136,47 +136,3 @@ export const btnScale = (size: "sm" | "lg") => css({
   paddingBlock: `calc(${button.paddingBlock} * ${scale[size]})`,
   fontSize: `calc(${button.fontSize} * ${scale[size]})`,
 }, { name: `btn-${size}` });
-
-export const Buttons = () => <>
-  <h1>Buttons</h1>
-  <h2>Filled</h2>
-  <button class="btn btn-full">Full Button</button>
-  <button class="btn btn-primary btn-full">Primary Button</button>
-  <button class="btn btn-accent btn-full">Accent Button</button>
-  <button class="btn btn-success btn-full">Success Button</button>
-  <button class="btn btn-warning btn-full">Warning Button</button>
-  <button class="btn btn-error btn-full">Error Button</button>
-  <button class="btn btn-info btn-full">Info Button</button>
-  <h2>Outline</h2>
-  <button class="btn btn-outline-primary btn-full">Outline Primary Button</button>
-  <button class="btn btn-outline-accent btn-full">Outline Accent Button</button>
-  <button class="btn btn-outline-success btn-full">Outline Success Button</button>
-  <button class="btn btn-outline-warning btn-full">Outline Warning Button</button>
-  <button class="btn btn-outline-error btn-full">Outline Error Button</button>
-  <button class="btn btn-outline-info btn-full">Outline Info Button</button>
-  <button class="btn btn-outline btn-full">Outline Button</button>
-  <h2>Soft</h2>
-  <button class="btn btn-soft-primary btn-full">Soft Primary Button</button>
-  <button class="btn btn-soft-accent btn-full">Soft Accent Button</button>
-  <button class="btn btn-soft-success btn-full">Soft Success Button</button>
-  <button class="btn btn-soft-warning btn-full">Soft Warning Button</button>
-  <button class="btn btn-soft-error btn-full">Soft Error Button</button>
-  <button class="btn btn-soft-info btn-full">Soft Info Button</button>
-  <button class="btn btn-soft btn-full">Soft Button</button>
-  <h2>Sizes</h2>
-  <button class="btn btn-lg">Large Button</button>
-  <button class="btn btn-sm">Small Button</button>
-  <h2>Rounded</h2>
-  <button class="btn btn-rounded">Rounded Button</button>
-  <h2>Disabled</h2>
-  <button disabled={true} class="btn btn-full">Disabled Button</button>
-  <button disabled={true} class="btn btn-full">Full Button</button>
-  <button disabled={true} class="btn btn-primary btn-full">Primary Button</button>
-  <button disabled={true} class="btn btn-accent btn-full">Accent Button</button>
-  <button disabled={true} class="btn btn-success btn-full">Success Button</button>
-  <button disabled={true} class="btn btn-warning btn-full">Warning Button</button>
-  <button disabled={true} class="btn btn-error btn-full">Error Button</button>
-  <button disabled={true} class="btn btn-info btn-full">Info Button</button>
-  <h2>Icon</h2>
-  <button class="btn btn-icon btn-rounded">I</button>
-</>
