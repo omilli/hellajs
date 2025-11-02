efefbfd04e27537115fa37f95f85ba7b7621a038
omilli <oli.millington@gmail.com>
2025-11-02 14:33:48 +0700

    chore: ionic starter

---
+++ examples/ionic/src/HomePage.tsx
@@ -0,0 +1,16 @@
+export function HomePage() {
+  return (
+    <ion-page>
+      <ion-header>
+        <ion-toolbar color="primary">
+          <ion-title class="px-4">HellaJS + Ionic + Tailwind</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content>
+        <div class="max-w-3xl mx-auto space-y-6 p-4">
+          <h1 class="text-3xl font-bold">Synchronized Theme System</h1>
+        </div>
+      </ion-content>
+    </ion-page>
+  );
+}
