6d44dff6f331595cfe2f423c57b34d75ddb1873f
GitHub Action <action@github.com>
2025-11-24 20:04:51 +0700

    chore: multiple bench tests

--- examples/bench/src-jsx/ActionButton.tsx
+++ examples/bench/src-jsx/ActionButton.tsx
@@ -1,20 +1,18 @@
-import { html } from "@hellajs/dom";
-
 interface ButtonProps {
   id: string;
   onClick: () => void;
   text: string;
 }
 
-export const ActionButton = (props: ButtonProps) => html`
+export const ActionButton = ((props: ButtonProps) => (
   <div class="col-sm-6">
     <button
-      id=${props.id}
-      on:click=${props.onClick}
+      id={props.id}
+      on:click={props.onClick}
       class="btn btn-primary btn-block col-md-6"
       type="button"
     >
-      ${props.text}
+      {props.text}
     </button>
   </div>
-`;
+));
---
+++ examples/bench/src-jsx/App.tsx
@@ -0,0 +1,33 @@
+import { mount, forEach } from "@hellajs/dom";
+import { rows, append, clear, create, swap, update, type RowSchema } from "./state";
+import { ActionButton } from "./ActionButton";
+import { Row } from "./Row";
+
+mount(<div id="main">
+  <div class="container">
+    <div class="jumbotron">
+      <div class="row">
+        <div class="col-md-6">
+          <h1>HellaJS Keyed</h1>
+        </div>
+        <div class="col-md-6">
+          <div class="row">
+            <ActionButton id="run" onClick={() => create(1000)} text="Create 1,000 rows" />
+            <ActionButton id="runlots" onClick={() => create(10000)} text="Create 10,000 rows" />
+            <ActionButton id="add" onClick={() => append(1000)} text="Append 1,000 rows" />
+            <ActionButton id="update" onClick={update} text="Update every 10th row" />
+            <ActionButton id="clear" onClick={clear} text="Clear" />
+            <ActionButton id="swaprows" onClick={swap} text="Swap Rows" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <table class="table table-hover table-striped test-rows">
+      <tbody>
+        {forEach(rows, (row: RowSchema) => <Row row={row} />)}
+      </tbody>
+    </table>
+    <span class="preloadicon glyphicon glyphicon-remove" ariaHidden="true"></span>
+  </div>
+</div>
+);
