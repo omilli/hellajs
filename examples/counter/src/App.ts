3a11079ea46b16fad44b9f7c54f640cd64627908
GitHub Action <action@github.com>
2026-05-04 21:29:21 +0700

    docs(tutorials): add initial implementation tutorial apps

--- examples/counter/src/App.ts
+++
@@ -1,8 +0,0 @@
-import { signal } from "@hellajs/core";
-import { html, mount } from "@hellajs/dom";
-
-const counter = signal(0);
-
-mount(html`
-  <button on:click=${() => counter(counter() + 1)}>${counter}</button>
-`);
