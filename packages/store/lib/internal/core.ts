365555316820730c1864a7f5440ea6d582724e47
GitHub Action <action@github.com>
2026-08-30 18:13:47 +0700

    feat(store): equals option

--- packages/store/lib/internal/core.ts
+++ packages/store/lib/internal/core.ts
@@ -1,2 +1,2 @@
 export { signal, computed, isFunction, isPlainObject, isObject } from "@hellajs/core";
-export type { Signal } from "@hellajs/core";
+export type { Signal, EqualsOptions } from "@hellajs/core";
